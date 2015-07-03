e22b7e3103631eaf655df9966c01c79e46ac1d62
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2015-07-03 15:02:41 +0200

    added a login redirect

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, Route, Navigation } from 'react-router';
 import HashHistory from 'react-router/lib/HashHistory';
 
 
@@ -57,17 +57,91 @@ class App extends React.Component {
         )
     }
 }
+let loggedIn = false;
+
+// http://rackt.github.io/react-router/tags/v1.0.0-beta3.html#Navigation
+//const Login = React.createClass({
+//    mixins: [Navigation],
+//    login() {
+//        loggedIn = true;
+//        //this.transitionTo('/inbox/spam');
+//    },
+//
+//    render() {
+//        return (<div>
+//                <div>Login</div>
+//                <button onClick={this.login}>Login</button>
+//            </div>
+//        );
+//    }
+//});
+
+// OR
+
+class Login extends React.Component {
+    static get contextTypes() {
+        return {
+            router: React.PropTypes.object.isRequired
+        };
+    }
+
+    constructor(props) {
+        super(props);
+        this.login = this.login.bind(this);
+    }
+
+    login() {
+        loggedIn = true;
+        this.context.router.transitionTo('/inbox/spam');
+    }
+
+    render() {
+        return (<div>
+                <div>Login</div>
+                <button onClick={this.login}>Login</button>
+            </div>
+        );
+    }
+}
+
+
+function requireAuth(nextState, transition) {
+    if (!loggedIn) {
+        transition.to('/login')
+    }
+}
+
+// TODO:
+// - Routes als React-Komponenten
+// - Step by step creating a login
+
+
+//const routes = (<Route path="/" component={App}>
+//    <Route path="about" component={About}/>
+//    <Route path="login" component={Login}/>
+//    <Route path="inbox" component={Inbox}>
+//        <Route path="spam"
+//               component={Spam}
+//               onEnter={requireAuth}
+//            />
+//        <Route path="messages/:id" component={Message}/>
+//    </Route>
+//    <Route path="*" component={NoMatch}/>
+//</Route>);
+
+// OR
 
 const routes = {
     path: '/',
     component: App,
     childRoutes: [
         {path: 'about', component: About},
+        {path: 'login', component: Login},
         {
             path: 'inbox',
             component: Inbox,
             childRoutes: [
-                {path: 'spam', component: Spam},
+                {path: 'spam', component: Spam, onEnter: requireAuth},
                 {path: 'message/:id', component: Message}
             ]
         },
