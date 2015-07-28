2166bfe64dd3a82b90191fe6903c5f70fc047127
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2015-07-28 15:22:45 +0200

    yo

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link, Route, Navigation } from 'react-router';
-import HashHistory from 'react-router/lib/HashHistory';
+import { Link, Route, Redirect, Navigation } from 'react-router';
 
 
 class NoMatch extends React.Component {
@@ -40,7 +39,17 @@ class Inbox extends React.Component {
 }
 
 class App extends React.Component {
+    static get contextTypes() {
+        return {
+            router: React.PropTypes.object.isRequired
+        };
+    }
+    gotoAbout() {
+      this.context.router.transitionTo('/about');
+    }
     render() {
+      const messageId = 48;
+      const path = `/inbox/message/${messageId}`;
         return (
             <div>
                 <h1>App</h1>
@@ -48,11 +57,15 @@ class App extends React.Component {
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/inbox">Inbox</Link></li>
                     <li><Link to="/inbox/spam">Spam</Link></li>
+                    <li><Link to={path}>My Message</Link></li>
                 </ul>
                 <div>
                     Children of App:
                     {this.props.children}
                 </div>
+                <div onClick={this.gotoAbout.bind(this)}>Go to About</div>
+                <div onClick={() => this.context.router.replaceWith('/inbox')}>Go to Inbox without creating a new history entry</div>
+                <div onClick={() => this.context.router.goBack()}>Go back</div>
             </div>
         )
     }
@@ -60,49 +73,49 @@ class App extends React.Component {
 let loggedIn = false;
 
 // http://rackt.github.io/react-router/tags/v1.0.0-beta3.html#Navigation
-//const Login = React.createClass({
-//    mixins: [Navigation],
-//    login() {
-//        loggedIn = true;
-//        //this.transitionTo('/inbox/spam');
-//    },
-//
-//    render() {
-//        return (<div>
-//                <div>Login</div>
-//                <button onClick={this.login}>Login</button>
-//            </div>
-//        );
-//    }
-//});
+const Login = React.createClass({
+   mixins: [Navigation],
+   login() {
+       loggedIn = true;
+       this.transitionTo('/inbox/spam');
+   },
+
+   render() {
+       return (<div>
+               <div>Login</div>
+               <button onClick={this.login}>Login</button>
+           </div>
+       );
+   }
+});
 
 // OR
 
-class Login extends React.Component {
-    static get contextTypes() {
-        return {
-            router: React.PropTypes.object.isRequired
-        };
-    }
-
-    constructor(props) {
-        super(props);
-        this.login = this.login.bind(this);
-    }
-
-    login() {
-        loggedIn = true;
-        this.context.router.transitionTo('/inbox/spam');
-    }
-
-    render() {
-        return (<div>
-                <div>Login</div>
-                <button onClick={this.login}>Login</button>
-            </div>
-        );
-    }
-}
+// class Login extends React.Component {
+//     static get contextTypes() {
+//         return {
+//             router: React.PropTypes.object.isRequired
+//         };
+//     }
+//
+//     constructor(props) {
+//         super(props);
+//         this.login = this.login.bind(this);
+//     }
+//
+//     login() {
+//         loggedIn = true;
+//         this.context.router.transitionTo('/inbox/spam');
+//     }
+//
+//     render() {
+//         return (<div>
+//                 <div>Login</div>
+//                 <button onClick={this.login}>Login</button>
+//             </div>
+//         );
+//     }
+// }
 
 
 function requireAuth(nextState, transition) {
@@ -116,37 +129,38 @@ function requireAuth(nextState, transition) {
 // - Step by step creating a login
 
 
-//const routes = (<Route path="/" component={App}>
-//    <Route path="about" component={About}/>
-//    <Route path="login" component={Login}/>
-//    <Route path="inbox" component={Inbox}>
-//        <Route path="spam"
-//               component={Spam}
-//               onEnter={requireAuth}
-//            />
-//        <Route path="messages/:id" component={Message}/>
-//    </Route>
-//    <Route path="*" component={NoMatch}/>
-//</Route>);
+const routes = (<Route path="/" component={App}>
+    <Route path="about" component={About}/>
+    <Route path="login" component={Login}/>
+    <Route path="inbox" component={Inbox}>
+        <Route path="spam"
+               component={Spam}
+               onEnter={requireAuth}
+            />
+        <Route path="message/:id" component={Message}/>
+    </Route>
+    <Route path="*" component={NoMatch}/>
+
+</Route>);
 
 // OR
 
-const routes = {
-    path: '/',
-    component: App,
-    childRoutes: [
-        {path: 'about', component: About},
-        {path: 'login', component: Login},
-        {
-            path: 'inbox',
-            component: Inbox,
-            childRoutes: [
-                {path: 'spam', component: Spam, onEnter: requireAuth},
-                {path: 'message/:id', component: Message}
-            ]
-        },
-        {path: '*', component: NoMatch}
-    ]
-};
+//const routes = {
+//    path: '/',
+//    component: App,
+//    childRoutes: [
+//        {path: 'about', component: About},
+//        {path: 'login', component: Login},
+//        {
+//            path: 'inbox',
+//            component: Inbox,
+//            childRoutes: [
+//                {path: 'spam', component: Spam, onEnter: requireAuth},
+//                {path: 'message/:id', component: Message}
+//            ]
+//        },
+//        {path: '*', component: NoMatch}
+//    ]
+//};
 
 export default routes;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Router } from 'react-router';
 import HashHistory from 'react-router/lib/HashHistory';
+import BrowserHistory from 'react-router/lib/BrowserHistory';
 
 import routes from './Routes';
 
-// https://github.com/rackt/react-router/issues/1332
 // Caution: new HashHistory()
-React.render(<Router history={new HashHistory()} children={routes}/>, document.getElementById('mount'));
+const history = new HashHistory();
+// const history = new BrowserHistory();
+
+// https://github.com/rackt/react-router/issues/1332
+ReactDOM.render(<Router history={history} children={routes}/>, document.getElementById('mount'));
